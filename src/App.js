30ad8199451f546e296f33dbdecbf83136e94914
Abenezer Nuro <abenuro@gmail.com>
2022-08-23 16:38:33 -0400

    new version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Vcard from "./Vcard.js";
+import QrCode from "./QrCode";
+import React, { useState, useEffect } from "react";
+
+
 
 function App() {
+
+const [contact, setPosts] = useState([]);
+useEffect(() => {
+  fetch("https://connect.artba.org/api/members?pageSize=100", {
+    Meethod: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Basic d2SuLwamTRQfEWqAuwBQ4zSTiSlq34mrICTaMeAIPS4=",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data['Results']);
+      setPosts(data["Results"]);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}, []);
+
+  const routeComponents = contact.map(
+    ({
+      FirstName,
+      Name,
+      UniqueID,
+      LastName,
+      Email,
+      Phone,
+      ParentMemberName,
+    }) => (
+      <Route
+        path={FirstName}
+        key={UniqueID}
+        element={
+          <Vcard
+            FirstName={FirstName}
+            LastName={LastName}
+            Email={Email}
+            ParentMemberName={ParentMemberName}
+            Phone={Phone}
+            Name={Name}
+          />
+        }
+      />
+    )
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>{routeComponents}</Routes>
+        <Routes>
+          <Route path="/" contact={contact} element={<QrCode />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Vcard.js
@@ -0,0 +1,54 @@
+import React from "react";
+import vcard from "vcard-generator";
+import { saveAs } from "file-saver";
+import { Button, Card, ListGroup } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Vcard({ FirstName, Name, LastName, Email, Phone, ParentMemberName }) {
+  var FileSaver = require("file-saver");
+  var blob = new Blob(
+    [
+      `BEGIN:VCARD
+VERSION:3.0
+N:${LastName};${FirstName};;;
+FN:${Name} ${LastName}
+EMAIL;type=INTERNET;type=pref:${Email}
+END:VCARD
+`,
+    ],
+    {
+      type: "text/vcard;charset=utf-8",
+    }
+  );
+  function saveAsFile() {
+    saveAs(blob, `${FirstName}.vcf`);
+  }
+  console.log(Phone);
+  return (
+    <Container>
+      <Row className="justify-center m-5">
+        <Col> </Col>
+        <Col sm={6}>
+          <Card border="info" style={{ width: "23rem", textAlign: "left" }}>
+            <Card.Header> {Name}</Card.Header>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>First Name: {FirstName}</ListGroup.Item>
+                <ListGroup.Item>Company: {ParentMemberName}</ListGroup.Item>
+
+                <ListGroup.Item>Email: {Email}</ListGroup.Item>
+                <ListGroup.Item>Phone: {Phone}</ListGroup.Item>
+
+                <ListGroup.Item></ListGroup.Item>
+              </ListGroup>
+              <Button onClick={saveAsFile}>Download vCard</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col> </Col>
+      </Row>
+    </Container>
+  );
+}
